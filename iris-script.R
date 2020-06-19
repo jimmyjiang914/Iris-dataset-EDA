ddc51798352bcad4d14be4801bf910c377c41dc0
Jimmy_Jiang <jimmyjiang914@gmail.com>
2020-06-19 01:28:34 -0400

    Add additional visualizations

--- iris-script.R
+++ iris-script.R
@@ -39,3 +39,60 @@ skim(iris) # Get detailed summary statistic
 iris %>%
   dplyr::group_by(Species) %>%
   skim()
+
+
+library(WVPlots) # Import for PairPlot
+
+PairPlot(iris, colnames(iris)[1:5],"Iris Pairplot") # A pairplot to visualize 
+# possible meaningful clusters
+
+
+library(plotly) # Import for interactive 3D plot
+
+# Creating a 3D plot for every combination of 3 columns out of the 4 features 
+# in the Iris data set
+fig1 <- plot_ly(iris, x = ~Sepal.Length, y = ~Sepal.Width, z = ~Petal.Length, color=~Species)
+fig1
+
+fig2 <- plot_ly(iris, x = ~Sepal.Width, y = ~Petal.Length, z = ~Petal.Width, color=~Species)
+fig2
+
+fig3 <- plot_ly(iris, x = ~Petal.Length, y = ~Petal.Width, z = ~Sepal.Length, color=~Species)
+fig3
+
+fig4 <- plot_ly(iris, x = ~Petal.Width, y = ~Sepal.Length, z = ~Sepal.Width, color=~Species)
+fig4
+
+
+
+# Recreating the above 3D plot combinations with a loop.
+# This can be very useful if we had more features to explore, manually creating 
+# 3D plots can be very time consuming if we're exploring many combinations.
+column_list <- colnames(iris)[1:4] # Obtain list of column names
+column_comb <- combn(column_list, 3) # Obtain all combinations of 3 columns
+
+fig <= NULL
+
+# Creating a function for specifying a certain paste
+fig.num <- function(n){
+  return(paste("fig", n, sep="."))
+}
+
+
+for (i in (1:4)) {
+  feature1 <- column_comb[1,i]
+  feature2 <- column_comb[2,i]
+  feature3 <- column_comb[3,i]
+  
+  print(paste(feature1, feature2, feature3, sep=" "))
+
+  fig <- plot_ly(iris, x = ~eval(parse(text=feature1)), y= ~eval(parse(text=feature2)), z = ~eval(parse(text=feature3)), color=~Species)
+  fig <- fig %>% layout(scene = list(xaxis = list(title = feature1),
+                                     yaxis = list(title = feature2),
+                                     zaxis = list(title = feature3)))
+  
+  assign(fig.num(i), fig)
+
+}
+
+fig.2
